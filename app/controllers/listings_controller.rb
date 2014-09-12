cbd139aa38fcd5c56d173123d07be4cf5560fc45
Nick Bernard <nick@nrbernard.com>
2014-09-12 10:28:09 -0700

    Create and show new listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,20 @@
 class ListingsController < ApplicationController
+  def new
+    @listing = Listing.new
+  end
 
+  def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      redirect_to listing_path(@listing), notice: "Park added!"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
 
 private
   def listing_params
