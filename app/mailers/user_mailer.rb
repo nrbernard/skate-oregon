f3b682c30f4b7809ad6345661d66f6eb6e9f0b85
Nick Bernard <nick@nrbernard.com>
2014-09-12 09:43:49 -0700

    Send users an email when they sign up

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ActionMailer::Base
+  default from: "nick@stayjoking.com"
+
+  def signup_confirmation(user)
+    @user = user
+    mail to: user.email, subject: "Sign Up Confirmation"
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :send_signup_confirmation
+
+private
+  def send_signup_confirmation
+    UserMailer.signup_confirmation(self).deliver
+  end
 end
